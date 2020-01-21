870410947e7a568ffe0c0862c83d705ec28482f7
micnncim <micnncim@gmail.com>
2020-01-21 15:21:56 +0900

    Update test of slack package (#12)

--- pkg/reporter/slack/slack_test.go
+++ pkg/reporter/slack/slack_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/nlopes/slack"
 	"go.uber.org/zap"
@@ -14,65 +15,92 @@ import (
 
 type fakeSlackClient struct {
 	buf *bytes.Buffer
+
+	postAttachmentsMessageCalls int // count of PostAttachmentsMessage method calls
 }
 
 func (s *fakeSlackClient) PostAttachmentsMessage(_ context.Context, _ string, attachments ...slack.Attachment) error {
+	s.postAttachmentsMessageCalls++
 	for _, a := range attachments {
 		for _, f := range a.Fields {
-			s.buf.WriteString(fmt.Sprintf("%s\n", f.Title))
+			s.buf.WriteString(fmt.Sprintf("%s\n%s\n\n", f.Title, f.Value))
 		}
 	}
 	return nil
 }
 
 func TestClient_Report(t *testing.T) {
+	fakeStarredAt := time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC)
+
 	type args struct {
 		repos []*github.Repo
 	}
 	tests := []struct {
-		name           string
-		args           args
-		maxAttachments int
-		want           string
-		wantErr        bool
+		name                            string
+		args                            args
+		maxAttachments                  int
+		want                            string
+		wantPostAttachmentsMessageCalls int
+		wantErr                         bool
 	}{
 		{
 			name: "report repos less than max",
 			args: args{
 				repos: []*github.Repo{
-					{Name: "repo1", Owner: "owner1"},
-					{Name: "repo2", Owner: "owner2"},
+					{Name: "repo1", Owner: "owner1", URL: "https://github.com/owner1/repo1", Description: "desc1", StarredAt: fakeStarredAt},
+					{Name: "repo2", Owner: "owner2", URL: "https://github.com/owner2/repo2", Description: "desc2", StarredAt: fakeStarredAt},
 				},
 			},
 			maxAttachments: maxAttachments,
 			want: `
 owner1/repo1
+https://github.com/owner1/repo1
+desc1
+Starred at 2020/04/01 00:00
+
 owner2/repo2
+https://github.com/owner2/repo2
+desc2
+Starred at 2020/04/01 00:00
+
 `[1:],
-			wantErr: false,
+			wantPostAttachmentsMessageCalls: 1,
+			wantErr:                         false,
 		},
 		{
 			name: "report repos more than max",
 			args: args{
 				repos: []*github.Repo{
-					{Name: "repo1", Owner: "owner1"},
-					{Name: "repo2", Owner: "owner2"},
+					{Name: "repo1", Owner: "owner1", URL: "https://github.com/owner1/repo1", Description: "desc1", StarredAt: fakeStarredAt},
+					{Name: "repo2", Owner: "owner2", URL: "https://github.com/owner2/repo2", Description: "desc2", StarredAt: fakeStarredAt},
 				},
 			},
 			maxAttachments: 1,
 			want: `
 owner1/repo1
+https://github.com/owner1/repo1
+desc1
+Starred at 2020/04/01 00:00
+
 owner2/repo2
+https://github.com/owner2/repo2
+desc2
+Starred at 2020/04/01 00:00
+
 `[1:],
-			wantErr: false,
+			wantPostAttachmentsMessageCalls: 2,
+			wantErr:                         false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			var orgMaxAttachments int
+			orgMaxAttachments, maxAttachments = maxAttachments, tt.maxAttachments
 			buf := &bytes.Buffer{}
+			fake := &fakeSlackClient{buf: buf}
 			c := &Client{
-				client: &fakeSlackClient{buf: buf},
+				client: fake,
 				logger: zap.NewNop(),
 			}
 			if err := c.Report(context.Background(), tt.args.repos); (err != nil) != tt.wantErr {
@@ -81,6 +109,10 @@ owner2/repo2
 			if buf.String() != tt.want {
 				t.Errorf("got: %s\nwant: %s", buf.String(), tt.want)
 			}
+			if fake.postAttachmentsMessageCalls != tt.wantPostAttachmentsMessageCalls {
+				t.Errorf("got: %d\nwant: %d", fake.postAttachmentsMessageCalls, tt.wantPostAttachmentsMessageCalls)
+			}
+			maxAttachments = orgMaxAttachments
 		})
 	}
 }
